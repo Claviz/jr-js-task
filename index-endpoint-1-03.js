1d359eed1160b510e220b9768d90de7ae82a85ee
Jurijs Konradi <66582002+jurikonradi@users.noreply.github.com>
2020-10-11 18:17:05 +0300

    Create index-endpoint-1-03.js
    added setInterval, the prise updates each minute

---
+++ index-endpoint-1-03.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+
+// test:
+// browser: http://localhost:3000/btc
+
+// Endpoint #1
+const companyPriceURL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+const UPDATE_INTERVAL = 1 * 60 * 1000; // 60 seconds
+
+let companyPrice = {};
+
+// request.get inside updateData, using setInterval:
+
+async function updateData(){
+    priceData = await request.get(companyPriceURL, (error, response, body) => {
+        if (error) console.log('Error: ', error);
+        console.log('statusCode:', response && response.statusCode);
+        companyPrice = JSON.parse(body);
+        console.log(`Price of ${companyPrice.chartName} is ${companyPrice.bpi.EUR.rate_float} ${companyPrice.bpi.EUR.code}`);
+    
+        app.get('/btc', function (request, response) {
+            response.send(`Price of ${companyPrice.chartName} is ${companyPrice.bpi.EUR.rate_float} ${companyPrice.bpi.EUR.code}`);
+        });
+    });
+}
+
+setInterval(updateData, UPDATE_INTERVAL);
+
+app.listen(3000);
+
+/* price object:
+{
+  time: {
+    updated: 'Oct 8, 2020 13:50:00 UTC',
+    updatedISO: '2020-10-08T13:50:00+00:00',
+    updateduk: 'Oct 8, 2020 at 14:50 BST'
+  },
+  disclaimer: 'This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org',
+  chartName: 'Bitcoin',
+  bpi: {
+    USD: {
+      code: 'USD',
+      symbol: '&#36;',
+      rate: '10,728.3811',
+      description: 'United States Dollar',
+      rate_float: 10728.3811
+    },
+    GBP: {
+      code: 'GBP',
+      symbol: '&pound;',
+      rate: '8,160.3285',
+      description: 'British Pound Sterling',
+      rate_float: 8160.3285
+    },
+    EUR: {
+      code: 'EUR',
+      symbol: '&euro;',
+      rate: '9,087.6576',
+      description: 'Euro',
+      rate_float: 9087.6576
+    }
+  }
+}
+*/
